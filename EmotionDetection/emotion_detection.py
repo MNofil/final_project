a83645d14af4bca592a0be448343fd54aff04d6f
MNofil <nofilsiddiqui300@gmail.com>
2025-08-21 12:53:52 -0700

    f

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,13 +5,24 @@ def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     
+    # Handle blank input
+    if not text_to_analyze.strip():
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+    
     # Input JSON
     input_json = { "raw_document": { "text": text_to_analyze } }
     
     # Send POST request
     response = requests.post(url, headers=headers, json=input_json)
     
-    # Handle blank input (status 400)
+    # Handle invalid requests (status_code = 400)
     if response.status_code == 400:
         return {
             "anger": None,
@@ -22,7 +33,6 @@ def emotion_detector(text_to_analyze):
             "dominant_emotion": None
         }
     
-    # Handle other errors
     if response.status_code != 200:
         return {"error": f"Request failed with status {response.status_code}"}
     
